a70dacd00174dfe6ff1a0fe2a497de8b9c6d9212
Suzanne M. Embury <suzanne.m.embury@manchester.ac.uk>
2021-07-26 04:10:55 +0100

    Initial import

---
+++ simploku.py
@@ -0,0 +1,99 @@
+# A simple Sudoku-type puzzle solver
+
+__copyright__ = "Copyright (C) 2021 The University of Manchester"
+
+import copy
+
+
+class SolutionNotFoundError(Exception):
+    """Exception raised when a solution to the input puzzle cannot be found."""
+    pass
+
+
+
+class SimplokuPuzzle(object):
+    """A simple Sudoku-style puzzle representation and solver."""
+    input_puzzle = []
+
+    def __init__(self, puzzle_to_solve):
+        self.input_puzzle = puzzle_to_solve
+        self.puzzle_size = len(self.input_puzzle)
+        self.rows = list(range(0, self.puzzle_size))
+        self.columns = self.rows
+
+        self.expected_values = list(range(1, self.puzzle_size+1))
+        self.puzzle_places = [(r, c) for r in self.rows for c in self.columns]
+
+    def solution(self):
+        return self.find_solution(self.input_puzzle)
+
+    def missing_values(self, puzzle, place):
+        (row, col) = place
+
+        row_values = [rmv for rmv in self.expected_values if rmv not in puzzle[row]]
+        column_values = [cmv for cmv in self.expected_values if not any(r[col] == cmv for r in puzzle)]
+        return [mv for mv in row_values if mv in column_values]
+
+    def find_solution(self, puzzle_to_solve):
+        # Find the places in the puzzle where there is a missing value
+        empty_places = [(r, c) for (r, c) in self.puzzle_places if puzzle_to_solve[r][c] is None]
+
+        if len(empty_places) == 0:
+            # The puzzle is already solved, and can be returned as the solution
+            return puzzle_to_solve
+
+        # Otherwise, we have some work to do
+
+        # Find the possible values that could be put into each empty place in the puzzle
+        candidate_values = {place: self.missing_values(puzzle_to_solve, place) for place in empty_places}
+
+        # If there are any empty places with no candidate values, then the puzzle does not have a solution
+        if any(len(candidate_values[p]) == 0 for p in empty_places):
+            raise SolutionNotFoundError()
+
+        # Otherwise, look for empty places which have only one possible value.
+        # Add that value to the puzzle, and then try to solve the remaining empty places.
+        places_to_solve_first = [place for place in empty_places if len(candidate_values[place]) == 1]
+        if len(places_to_solve_first) > 0:
+            (row, col) = places_to_solve_first[0]
+            puzzle_to_solve[row][col] = candidate_values[(row, col)][0]
+            return self.find_solution(puzzle_to_solve)
+
+        # Otherwise, the empty cells all have more than one candidate value.  We just have to try these values
+        # in turn until we find one that works.  If none of them work then the puzzle is unsolvable.
+        # We know there will always be at least one such place if we reach this point (unless there is a bug in the code).
+        places_to_solve_next = [place for place in empty_places if len(candidate_values[place]) < 1]
+        if len(places_to_solve_next) > 0:
+            # Try placing each candidate value in turn and then solving the resulting problem.
+            (row, col) = places_to_solve_next[0]
+            for candidate in candidate_values[(row, col)]:
+                try:
+                    # Make a copy of the puzzle we are trying to solve so we can try out a possible solution without
+                    # damaging the original puzzle, in case we need to go back and try again.
+                    puzzle = copy.deepcopy(puzzle_to_solve)
+                    puzzle[row][col] = candidate
+                    return self.find_solution(puzzle)
+                except SolutionNotFoundError:
+                    # If the current candidate value didn't lead to a solution, then try the next one.
+                    continue
+
+        # If we reach this point, then something weird has happened and we were not able to find a solution.
+        raise SolutionNotFoundError()
+
+
+def main():
+    puzzle_start_state = [[None, 2, 3, 4,    5, 6,    7,    8, 9],
+                          [2,    3, 4, 5,    6, 7,    8,    9, 1],
+                          [3,    4, 5, None, 7, None, 9,    1, 2],
+                          [4,    5, 6, 7,    8, 9,    1,    2, 3],
+                          [5,    6, 7, 8,    9, 1,    2,    3, 4],
+                          [6,    7, 8, 9,    1, 2,    3,    4, 5],
+                          [7,    8, 9, None, 2, None, 4,    5, 6],
+                          [8,    9, 1, 2,    3, 4,    5,    6, 7],
+                          [None, 1, 2, None, 4, 5,    None, 7, 8]]
+    puzzle = SimplokuPuzzle(puzzle_start_state)
+    print(puzzle.solution())
+
+
+if __name__ == "__main__":
+    main()
---
+++ test_simploku.py
@@ -0,0 +1,169 @@
+
+__copyright__ = "Copyright (C) 2021 The University of Manchester"
+
+
+from unittest import TestCase
+from nose.tools import assert_equals, assert_in, assert_raises
+
+from simploku import SimplokuPuzzle, SolutionNotFoundError
+
+
+def check_contains_exactly(actual_contents, expected_contents):
+    assert_equals(sorted(actual_contents), expected_contents)
+
+
+def assert_puzzle_solution_is(puzzle, solution):
+    puzzle = SimplokuPuzzle(puzzle)
+    assert_equals(puzzle.solution(), solution)
+
+
+def assert_puzzle_solution_is_one_of(puzzle, *solutions):
+    puzzle = SimplokuPuzzle(puzzle)
+    assert_in(puzzle.solution(), solutions)
+
+
+def assert_no_solution_exists(puzzle):
+    puzzle = SimplokuPuzzle(puzzle)
+    with assert_raises(SolutionNotFoundError):
+        puzzle.solution()
+
+
+class SimplokuTest(TestCase):
+
+    def test_return_a_solved_one_by_one_puzzle_unchanged(self):
+        puzzle_to_solve = [[1]]
+        assert_puzzle_solution_is(puzzle_to_solve, puzzle_to_solve)
+
+    def should_return_a_solved_two_by_two_puzzle_unchanged(self):
+        puzzle_to_solve = [[1, 2], [2, 1]]
+        assert_puzzle_solution_is(puzzle_to_solve, puzzle_to_solve)
+
+    def should_solve_a_two_by_two_puzzle_with_1_gap_top_left(self):
+        assert_puzzle_solution_is([[None, 1],
+                                   [1,    2]],
+                                  [[2, 1],
+                                   [1, 2]])
+
+    def should_solve_a_different_two_by_two_puzzle_with_1_gap_top_left(self):
+        assert_puzzle_solution_is([[None, 2],
+                                   [2, 1]],
+                                  [[1, 2],
+                                   [2, 1]])
+
+    def should_solve_a_two_by_two_puzzle_with_1_gap_top_right(self):
+        assert_puzzle_solution_is([[1, None],
+                                   [2, 1]],
+                                  [[1, 2],
+                                   [2, 1]])
+
+    def should_solve_a_two_by_two_puzzle_with_1_gap_bottom_left(self):
+        assert_puzzle_solution_is([[1, 2],
+                                   [None, 1]],
+                                  [[1, 2],
+                                   [2, 1]])
+
+    def should_solve_a_two_by_two_puzzle_with_1_gap_bottom_right(self):
+        assert_puzzle_solution_is([[1, 2],
+                                   [2, None]],
+                                  [[1, 2],
+                                   [2, 1]])
+
+    def should_solve_a_three_by_three_puzzle_with_1_gap_top_left(self):
+        assert_puzzle_solution_is([[None, 2, 3],
+                                   [2,    3, 1],
+                                   [3,    1, 2]],
+                                  [[1, 2, 3],
+                                   [2, 3, 1],
+                                   [3, 1, 2]])
+
+    def should_solve_a_different_three_by_three_puzzle_with_1_gap_top_left(self):
+        assert_puzzle_solution_is([[None, 3, 1],
+                                   [3, 1, 2],
+                                   [1, 2, 3]],
+                                  [[2, 3, 1],
+                                   [3, 1, 2],
+                                   [1, 2, 3]])
+
+    def should_solve_a_different_three_by_three_puzzle_with_1_gap_in_the_centre(self):
+        assert_puzzle_solution_is([[2, 3,    1],
+                                   [3, None, 2],
+                                   [1, 2,    3]],
+                                  [[2, 3, 1],
+                                   [3, 1, 2],
+                                   [1, 2, 3]])
+
+    def should_solve_a_nine_by_nine_puzzle_with_4_gaps(self):
+        assert_puzzle_solution_is([[None, 2, 3,    4, 5, 6,    7, 8, 9],
+                                   [2,    3, 4,    5, 6, 7,    8, 9, 1],
+                                   [3,    4, 5,    6, 7, None, 9, 1, 2],
+                                   [4,    5, 6,    7, 8, 9,    1, 2, 3],
+                                   [5,    6, 7,    8, 9, 1,    2, 3, 4],
+                                   [6,    7, 8,    9, 1, 2,    3, 4, 5],
+                                   [7,    8, None, 1, 2, None, 4, 5, 6],
+                                   [8,    9, 1,    2, 3, 4,    5, 6, 7],
+                                   [9, 1, 2, 3,    4, 5, 6, 7, 8]],
+                                  [[1, 2, 3, 4, 5, 6, 7, 8, 9],
+                                   [2, 3, 4, 5, 6, 7, 8, 9, 1],
+                                   [3, 4, 5, 6, 7, 8, 9, 1, 2],
+                                   [4, 5, 6, 7, 8, 9, 1, 2, 3],
+                                   [5, 6, 7, 8, 9, 1, 2, 3, 4],
+                                   [6, 7, 8, 9, 1, 2, 3, 4, 5],
+                                   [7, 8, 9, 1, 2, 3, 4, 5, 6],
+                                   [8, 9, 1, 2, 3, 4, 5, 6, 7],
+                                   [9, 1, 2, 3, 4, 5, 6, 7, 8]])
+
+    def should_solve_a_nine_by_nine_puzzle_with_7_gaps(self):
+        assert_puzzle_solution_is([[None, 2, 3,    4, 5, None, 7, 8, 9],
+                                   [2,    3, 4,    5, 6, 7,    8, 9, 1],
+                                   [3,    4, 5,    6, 7, None, 9, 1, None],
+                                   [4,    5, 6,    7, 8, 9,    1, 2, 3],
+                                   [5,    6, 7,    8, 9, 1,    2, 3, 4],
+                                   [6,    7, 8,    9, 1, 2,    3, 4, 5],
+                                   [7,    8, None, 1, 2, None, 4, 5, None],
+                                   [8,    9, 1,    2, 3, 4,    5, 6, 7],
+                                   [9,    1, 2,    3, 4, 5,    6, 7, 8]],
+                                  [[1, 2, 3, 4, 5, 6, 7, 8, 9],
+                                   [2, 3, 4, 5, 6, 7, 8, 9, 1],
+                                   [3, 4, 5, 6, 7, 8, 9, 1, 2],
+                                   [4, 5, 6, 7, 8, 9, 1, 2, 3],
+                                   [5, 6, 7, 8, 9, 1, 2, 3, 4],
+                                   [6, 7, 8, 9, 1, 2, 3, 4, 5],
+                                   [7, 8, 9, 1, 2, 3, 4, 5, 6],
+                                   [8, 9, 1, 2, 3, 4, 5, 6, 7],
+                                   [9, 1, 2, 3, 4, 5, 6, 7, 8]])
+
+    def should_find_a_3_by_3_solution_when_more_than_one_exists(self):
+        assert_puzzle_solution_is_one_of([[None, None, 3],
+                                          [None, None, 2],
+                                          [None, None, 1]],
+                                         [[1, 2, 3],
+                                          [3, 1, 2],
+                                          [2, 3, 1]],
+                                         [[2, 1, 3],
+                                          [1, 3, 2],
+                                          [3, 2, 1]])
+
+    def should_find_a_4_by_4_solution_when_more_than_one_exists(self):
+        assert_puzzle_solution_is_one_of([[None, None, 3, 4],
+                                          [None, None, 4, 3],
+                                          [3,    4,    1, 2],
+                                          [4,    3,    2, 1]],
+                                         [[1, 2, 3, 4],
+                                          [2, 1, 4, 3],
+                                          [3, 4, 1, 2],
+                                          [4, 3, 2, 1]],
+                                         [[2, 1, 3, 4],
+                                          [1, 2, 4, 3],
+                                          [3, 4, 1, 2],
+                                          [4, 3, 2, 1]])
+
+    def should_reject_an_impossible_puzzle(self):
+        assert_no_solution_exists([[None, None, 3],
+                                   [None, None, 2],
+                                   [1,    None, None]])
+
+    def should_reject_an_invalid_puzzle(self):
+        assert_no_solution_exists([[None, 3,    3],
+                                   [None, None, None],
+                                   [None, None, None]])
+
